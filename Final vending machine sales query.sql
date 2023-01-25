b4e33ba95840d9da775b99c608678a7c5bcd5086
KnickKnackData <116595958+KnickKnackData@users.noreply.github.com>
2023-01-24 21:00:34 -0600

    Update Final vending machine sales query.sql

--- Final vending machine sales query.sql
+++ Final vending machine sales query.sql
@@ -1,5 +1,5 @@
 -- The purpose of this project is to use the data to understand trends, patterns and revenue from other machines to determine where the next location of our new vending machine will be placed  
--- user behavior, user consumption habits, and overall preferences by consumers at different locations also place a factor
+-- user behavior, user consumption habits, and overall preferences by consumers at different locations also play a factor
 
 SELECT *
 FROM vending_machine_sales;
